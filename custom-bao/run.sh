be52ed497bc0bb59105f904187c9c9704b271cf5
luiss07 <luigi.delleva@gmail.com>
2025-06-16 17:01:11 +0100

    minor

--- custom-bao/run.sh
+++ custom-bao/run.sh
@@ -5,6 +5,6 @@ bash $ROOT_DIR/build_openSBI.sh
 qemu-system-riscv64 -nographic\
     -M virt -cpu rv64 -m 4G -smp 4\
     -bios $TOOLS_DIR/bin/opensbi.elf\
+    -device virtio-net-device,netdev=net0\
+    -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22\
     -device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3
-    # -device virtio-net-device,netdev=net0\
-    # -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22\
